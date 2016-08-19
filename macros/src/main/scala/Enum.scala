78eead82e71c1d1a98b85dfe579be466c010deff
Richard Gomes <rgomes.info@gmail.com>
2016-08-19 22:16:11 +0100

    Move trial code to comments.

--- macros/src/main/scala/Enum.scala
+++ macros/src/main/scala/Enum.scala
@@ -5,19 +5,6 @@ import scala.meta._
 @compileTimeOnly("@enum not expanded")
 class enum extends scala.annotation.StaticAnnotation {
   inline def apply(defn: Any) = meta {
-    //XXX val q"""object $holder { ..$stats }""" = defn
-    //XXX val enum =
-    //XXX   q"""
-    //XXX     object $holder {
-    //XXX       sealed abstract class enum(val id: Int, val name: String) extends Ordered[enum] {
-    //XXX         def compare(that: enum) = this.id - that.id
-    //XXX       }
-    //XXX       $expr(...$stats) extends enum(0, "fake")
-    //XXX     }
-    //XXX   """
-    //XXX println(enum)
-    //XXX enum
-
     val q"""object $holder { ..$mods object $entry extends $template }""" = defn
     val enum =
       q"""
@@ -33,6 +20,20 @@ class enum extends scala.annotation.StaticAnnotation {
 }
 
 /*
+    val q"""object $holder { ..$stats }""" = defn
+    val enum =
+      q"""
+        object $holder {
+          sealed abstract class enum(val id: Int, val name: String) extends Ordered[enum] {
+            def compare(that: enum) = this.id - that.id
+          }
+          $expr(...$stats) extends enum(0, "fake")
+        }
+      """
+    println(enum)
+    enum
+
+
     println("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv")
     println(holder)
     println(mods)
